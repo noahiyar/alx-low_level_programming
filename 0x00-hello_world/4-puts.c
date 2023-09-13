62b52b5d5fa09a82ba1396891cf3945ed932865f
noahiyar <noahaboiyar@gmail.com>
2023-09-13 16:23:05 -0700

    betty is the problem

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
 * this is a program to print characters
 * using puts command in c
 */
-	// initiate the main integer
+
 int main(void)
 {
-	// using puts to output characters
 	puts("\"Programming is like building a multilingual puzzle");
-	// the initiated integer returns a zero
+
 	return (0);
 }
