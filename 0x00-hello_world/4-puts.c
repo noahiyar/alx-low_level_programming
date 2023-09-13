39b73fb9fc4175948113892e048e2f4f399bfcfd
noahiyar <noahaboiyar@gmail.com>
2023-09-13 14:23:30 -0700

    refix on hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/*
+ * this is a program to print characters
+ * using puts command in c
+ */
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
