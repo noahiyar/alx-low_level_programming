01b446c3bf1ae50cd29467f263d81fd80248de6d
noahiyar <noahaboiyar@gmail.com>
2023-09-13 16:00:01 -0700

    repairs on hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,11 @@
 * this is a program to print characters
 * using puts command in c
 */
-
+/* initiate the main integer */
 int main(void)
 {
+/* using puts to output characters */
 	puts("\"Programming is like building a multilingual puzzle");
+/* the initiated integer returns a zero */
 	return (0);
 }
