b6935f7a161734782f6f64976f6aed559f5b27b7
noahiyar <noahaboiyar@gmail.com>
2023-09-14 02:57:41 -0700

    c file for hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * c program using printf to send output
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
